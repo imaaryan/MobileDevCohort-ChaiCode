3ce640f0904bd521e8ba91ab0f4d8b5822052d97
imaaryan <imaaryanpandey@gmail.com>
2026-05-09 16:11:57 +0530

    feat: integrate Tailwind CSS and Flowbite for styling
    - Added Tailwind CSS and Flowbite dependencies in package.json.
    - Created a new Card component for displaying content with Flowbite styles.
    - Updated App component to include the new Card component.
    - Configured Vite to use Tailwind CSS plugin.
    - Added global styles in index.css for Tailwind and Flowbite themes.
    - Removed unused assets (hero.png, react.svg, vite.svg) to clean up the project.

---
+++ Day1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'flowbite';
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <>
+    <Card imgLink="https://images.unsplash.com/photo-1773227881000-07b791984bf1?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" title="Streamlining your design process today." desc="In today’s fast-paced digital landscape, fostering seamless collaboration among Developers and IT Operations." cta="Read More" />
+    </>
+  )
+}
+
+export default App
